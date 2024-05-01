3d8c9b49964f8fb561b720201200f02fcc6e84ec
rshal <heiphin7and2@gmail.com>
2024-05-01 21:46:31 +0500

    Modified Multhithread unZip processing

--- src/main/java/com/file/uploader/threads/FileProcessor.java
+++ src/main/java/com/file/uploader/threads/FileProcessor.java
@@ -1,26 +1,20 @@
 package com.file.uploader.threads;
 
-
 import com.file.uploader.utils.UploadPaths;
-import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 
-@Component
-public class FileProcessor implements Runnable{
+public class FileProcessor implements Runnable {
     private List<File> fileList;
 
-    public FileProcessor (List<File> fileList) {
+    public FileProcessor(List<File> fileList) {
         this.fileList = fileList;
     }
 
     @Override
     public void run() {
-
+        // Цикл для перебора файлов из zip-a
         for (File file: fileList) {
             String name = file.getName();
             String fileExtension = ""; // Расширение файла
@@ -29,36 +23,41 @@ public class FileProcessor implements Runnable{
             int dotIndex = name.lastIndexOf('.');
             if (dotIndex > 0) {
                 fileExtension = name.substring(dotIndex + 1);
+                fileExtension = fileExtension.toLowerCase();
             }
+
+            System.out.println("File Name: " + name + ", Extension: " + fileExtension);
 
             // Путь для сохранения
             String destinationPath = "";
 
-            System.out.println(fileExtension.toLowerCase());
+            System.out.println(destinationPath);
 
-            destinationPath = switch (fileExtension.toLowerCase()) {
-                case "jpeg", "jpg" -> UploadPaths.IMAGES_JPEG_PATH;
-                case "png" -> UploadPaths.IMAGES_PNG_PATH;
-                case "pdf" -> UploadPaths.PDF_PATH;
-                case "json" -> UploadPaths.JSON_PATH;
-                case "txt" -> UploadPaths.TEXTS_PATH;
-                default -> UploadPaths.OTHERS_PATH;
-            };
+            if (fileExtension.equals("jpeg") || fileExtension.equals("jpg")) {
+                destinationPath = UploadPaths.IMAGES_JPEG_PATH;
+            } else if (fileExtension.equals("png")) {
+                destinationPath = UploadPaths.IMAGES_PNG_PATH;
+            } else if (fileExtension.equals("pdf")) {
+                destinationPath = UploadPaths.PDF_PATH;
+            } else if(fileExtension.equals("json")) {
+                destinationPath = UploadPaths.JSON_PATH;
+            } else if (fileExtension.equals("txt")) {
+                destinationPath = UploadPaths.TEXTS_PATH;
+            }
 
             // Далее, в зависимости от файла мы должны закинуть его в одну из директорий
             // Создание файла для перемещения
             File destinationFile = new File(destinationPath + name);
 
             // Перемещение файла
-            try {
-                Files.copy(file.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                Files.deleteIfExists(file.toPath());
+            if (file.renameTo(destinationFile)) {
+                // Успешно перемещен
                 System.out.println("Файл " + name + " успешно перемещен в " + destinationPath);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Ошибка при перемещении файла " + name);
+            } else {
+                // Возникла ошибка при перемещении файла
+                System.out.println("Ошибка при перемещении файла: " + name);
             }
-        }
 
+        }
     }
 }
