62897f9de80a59788850f2a44adda14c7f760989
rshal <heiphin7and2@gmail.com>
2024-05-01 19:11:05 +0500

    Start of developing V2 of API

--- src/main/java/com/file/uploader/controller/MainController.java
+++ src/main/java/com/file/uploader/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.file.uploader.controller;
 
+import com.file.uploader.threads.FileProcessor;
 import com.file.uploader.utils.TypeChecker;
 import com.file.uploader.utils.UploadPaths;
 import com.file.uploader.utils.ZipUtils;
@@ -79,58 +80,12 @@ public class MainController {
                 List<File> filesList;
 
 
-                try {
-                    filesList = ZipUtils.unZip(fullPath);
-                } catch (IOException e) {
-                    System.out.println(e);
-                    return new ResponseEntity<>("Ошибка при распаковке архива", HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-
-                // Цикл для перебора файлов из zip-a
-                for (File file: filesList) {
-                    String name = file.getName();
-                    String fileExtension = ""; // Расширение файла
-
-                    // Получение расширения файла
-                    int dotIndex = name.lastIndexOf('.');
-                    if (dotIndex > 0) {
-                        fileExtension = name.substring(dotIndex + 1);
-                        fileExtension.toLowerCase();
-                    }
-
-                    // Путь для сохранения
-                    String destinationPath = "";
-
-                    System.out.println(destinationPath);
-
-                    if(fileExtension.equals("jpeg") || fileExtension.equals("jpg")) {
-                        destinationPath = UploadPaths.IMAGES_JPEG_PATH;
-                    } else if (fileExtension.equals("png")) {
-                        destinationPath = UploadPaths.IMAGES_PNG_PATH;
-                    } else if (fileExtension.equals("pdf")) {
-                        destinationPath = UploadPaths.PDF_PATH;
-                    } else if(fileExtension.equals("json")) {
-                        destinationPath = UploadPaths.JSON_PATH;
-                    } else if (fileExtension.equals("txt")) {
-                        destinationPath = UploadPaths.TEXTS_PATH;
-                    }
-
-                    // Далее, в зависимости от файла мы должны закинуть его в одну из директорий
-                    // Создание файла для перемещения
-                    File destinationFile = new File(destinationPath + name);
-
-                    // Перемещение файла
-                    if (file.renameTo(destinationFile)) {
-                        // Успешно перемещен
-                        System.out.println("Файл " + name + " успешно перемещен в " + destinationPath);
-                    } else {
-                        // Возникла ошибка при перемещении файла
-                        System.out.println("Ошибка при перемещении файла " + name);
-                    }
-
+                try{
+                    filesList = ZipUtils.unZip(fullPath, 6);
+                }catch (IOException e){
+                    return new ResponseEntity<>("Ошибка при разархивировании файла", HttpStatus.INTERNAL_SERVER_ERROR);
                 }
             }
-
         }
 
         return ResponseEntity.ok("Файлы успешно сохранены!");
--- src/main/java/com/file/uploader/utils/ZipUtils.java
+++ src/main/java/com/file/uploader/utils/ZipUtils.java
@@ -1,24 +1,29 @@
 package com.file.uploader.utils;
 
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipFile;
 import org.springframework.stereotype.Component;
-import org.apache.commons.compress.archivers.ArchiveEntry;
-import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.utils.IOUtils;
 
 
-import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.io.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static org.apache.commons.compress.utils.IOUtils.*;
 
 
 @Component
 public class ZipUtils {
-    /*
-
-    */
-
-    // Метод для разархивирования и возвращении всех его файлов
+    /*   V1
     public static List<File> unZip(String zipFilePath) throws IOException {
         List<File> fileList = new ArrayList<>();
         try (ZipArchiveInputStream zipInputStream = new ZipArchiveInputStream(new FileInputStream(zipFilePath))) {
@@ -27,12 +32,54 @@ public class ZipUtils {
                 if (!entry.isDirectory()) {
                     File outputFile = new File(entry.getName());
                     try (OutputStream outputStream = new FileOutputStream(outputFile)) {
-                        IOUtils.copy(zipInputStream, outputStream);
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = zipInputStream.read(buffer)) > 0) {
+                            outputStream.write(buffer, 0, length);
+                        }
+                        fileList.add(outputFile);
                     }
-                    fileList.add(outputFile);
                 }
             }
         }
         return fileList;
     }
+     */
+
+    public static List<File> unZip(String zipFilePath, int numThreads) throws IOException {
+        List<File> fileList = Collections.synchronizedList(new ArrayList<>());
+        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+
+        try (ZipFile zipFile = new ZipFile(zipFilePath)) {
+            Enumeration<? extends ZipArchiveEntry> entries = zipFile.getEntries();
+            while (entries.hasMoreElements()) {
+                ZipArchiveEntry entry = entries.nextElement();
+                if (!entry.isDirectory()) {
+                    executorService.execute(() -> {
+                        try (InputStream inputStream = zipFile.getInputStream(entry)) {
+                            File outputFile = new File(entry.getName());
+                            try (OutputStream outputStream = new FileOutputStream(outputFile)) {
+                                IOUtils.copy(inputStream, outputStream);
+                                fileList.add(outputFile);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return fileList;
+    }
+
 }
