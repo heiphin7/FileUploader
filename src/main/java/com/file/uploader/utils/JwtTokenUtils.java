0baf18ffe753acd135cbd8e4b4cd2b0ea327bd1d
rshal <heiphin7and2@gmail.com>
2024-05-03 11:07:07 +0500

    Created methods for JwtTokenUtils (generate, claims & username from tokena)

--- src/main/java/com/file/uploader/utils/JwtTokenUtils.java
+++ src/main/java/com/file/uploader/utils/JwtTokenUtils.java
@@ -1,11 +1,19 @@
 package com.file.uploader.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
+@Component
 public class JwtTokenUtils {
 
     @Value("${jwt.secret}")
@@ -13,5 +21,41 @@ public class JwtTokenUtils {
 
     @Value("${jwt.lifetime}")
     private Duration lifetime; // 1 hour
+
+    public String generateToken (UserDetails user) {
+
+        Map<String, Object> claims = new HashMap<>();
+
+        List<String> rolesList = user.getAuthorities().stream().map(
+                GrantedAuthority::getAuthority
+                ).collect(Collectors.toList());
+
+        claims.put("roles", rolesList);
+
+        Date issuedAt = new Date(); // Дата подписания
+        Date expiredDate = new Date(issuedAt.getTime() + lifetime.toMillis()); // Дата истечения токена
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiredDate)
+                .signWith(SignatureAlgorithm.HS256, secret) // secret & Hash algorithm
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getAllClaimsFromToken(token).getSubject(); // Subject is Username
+    }
+
+    public List<?> getRoles(String token){
+        return getAllClaimsFromToken(token).get("roles", List.class);
+    }
+
+
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret)
+                .parseClaimsJws(token).getBody();
+    }
 
 }
